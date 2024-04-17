c6014e413c79fb61f208fa2728d346a05a2badb9
Aditya swaroop <143704567+Aditya-swaroop-11@users.noreply.github.com>
2024-04-17 11:27:23 +0000

    Try 2

--- problem_1.py
+++ problem_1.py
@@ -1,17 +1,15 @@
-def get_tshirt(x):
-    shirt=["p","c","j"]
-    for i in (shirt):
-        if(i==x):
-            get_name()
-            print("Hi" ,name,"Brand you looking for is available in our store")
-            break
-        else:
-            get_name()
-            print("Hi" ,name,"Unfortunately Brand you looking for is not available in our store")
-            break
-
 def get_name():
-    name=input("enter the name")
-    
-a=input("enter the brand")
-get_tshirt(a)
+    a=input("Enter your name:")
+    return a
+def get_tshirt():
+    user=get_name()
+    brand_name=input("Enter the brand name you are searching for:")
+    brand=["nike","adidas","hrx","puma","Peter England","Rebook"]
+    for i in brand:
+        if brand_name==i:
+            print("Hi" ,{user}, "the brand you are looking for is available in our store ")
+            break
+    else:
+        print("Hi" ,{user},"Unfortunately the brand you are looking for is not available in our store")
+
+get_tshirt()
