7edbb05e3b2e257329395d615c492a41688c9de7
Aditya swaroop <143704567+Aditya-swaroop-11@users.noreply.github.com>
2024-04-17 11:51:44 +0000

    Try-2

--- problem_2.py
+++ problem_2.py
@@ -5,19 +5,23 @@ def get_tshirt():
     user=get_name()
     brand_name=input("Enter the brand name you are searching for:")
     brand=["nike","adidas","hrx","puma","peter england","rebook"]
-    size_=["S","M","L","XL","XXL"]
+    
     for i in brand:
         if brand_name==i:
             print("Hi" ,user, ",The brand you are looking for is available in our store ")
             break
     else:
         print("Hi" ,user,",Unfortunately the brand you are looking for is not available in our store")
-    size=input("Enter the size of your t-shirt")
-    for i in size_:
-        if size==i:
-            print("Hi" ,user, ",The size you are looking for is available in our store ")
-            break
-    else:
-        print("Hi" ,user, ",Unfortunately the size you are looking for is not available in our store")
+    
+    for i in brand:
+        if brand_name==i:
+            size=input("Enter the size of your t-shirt")
+            size_=["S","M","L","XL","XXL"]
+            for i in size_:
+                if size==i:
+                    print("Hi" ,user, ",The size you are looking for is available in our store ")
+                    break
+            else:
+                print("Hi" ,user, ",Unfortunately the size you are looking for is not available in our store")
 
 get_tshirt()
