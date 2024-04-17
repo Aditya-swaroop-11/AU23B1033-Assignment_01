1fc534f31f9ed94ebdd071ffff3c336a102d5d33
Aditya swaroop <143704567+Aditya-swaroop-11@users.noreply.github.com>
2024-04-17 11:30:42 +0000

    Try-3

--- problem_1.py
+++ problem_1.py
@@ -1,15 +1,15 @@
-def get_name():
-    a=input("Enter your name:")
+def get_name(): #defining a function 
+    a=input("Enter your name:") #user input of name
     return a
-def get_tshirt():
-    user=get_name()
-    brand_name=input("Enter the brand name you are searching for:")
-    brand=["nike","adidas","hrx","puma","Peter England","Rebook"]
-    for i in brand:
-        if brand_name==i:
-            print("Hi" ,{user}, "the brand you are looking for is available in our store ")
+def get_tshirt(): #defining another function
+    user=get_name() #calling the first function
+    brand_name=input("Enter the brand name you are searching for:") #user input for searching
+    brand=["nike","adidas","hrx","puma","Peter England","Rebook"] #Brand available in store
+    for i in brand: 
+        if brand_name==i: #cheaking for the brand
+            print("Hi" ,user, ",the brand you are looking for is available in our store ")
             break
     else:
-        print("Hi" ,{user},"Unfortunately the brand you are looking for is not available in our store")
+        print("Hi" ,user,",Unfortunately the brand you are looking for is not available in our store")
 
-get_tshirt()
+get_tshirt() 
